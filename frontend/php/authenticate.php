39556f20be9cf18ce1b614afb4151a34c56ac6eb
n01069097 <n01069097@ospreys.unf.edu>
2017-06-23 16:09:22 -0400

    Update authenticate.php

--- frontend/php/authenticate.php
+++ frontend/php/authenticate.php
@@ -1,5 +1,30 @@
 <?php
-  echo "THis will be a simple authenication page. Only backend stuff here"
+	session_start();
 
+	$myuser = $_POST["username"];
+	$mypass = $_POST["password"];
 
- ?>
+	$_SESSION["username"] = $username;
+
+    $query = "SELECT username, password FROM Users";
+    $result = mysql_query($databaseConnect, $query);
+
+    while ($record = mysql_fetch_array($result)) {
+        $username = $record[0];
+		    $password = $record[1];
+
+        if($username == $myuser)
+        {
+          if($password == $mypass)
+          {
+            header("Location: home.php");
+            exit;
+          }
+          else {
+            header("Location: signIn.php?error=2;
+            exit;
+          }
+        }
+	  }
+	mysqli_close($databaseConnect);
+?>
